cdb2030d740145bb3fdadd40feac71a5e77a7e4a
user <laura.teraude@gmail.com>
2020-07-28 19:12:57 +0300

    20200728_18_30

---
+++ MyFirstJava/Variables.java
@@ -0,0 +1,21 @@
+
+
+public class Variables {
+	public static void main(String[] args) {
+	System.out.println("Hello Worls!");
+	
+         byte b = 127;
+	 short ss = 32000;
+	 int x = 4;
+	 long bigNumber = 100L;
+
+	 float y = 3.14f;
+	 double z = 2.72;
+
+	 char c = 'A';
+	 boolean decision = true;
+
+System.out.println("The Byte primitive = " +b);
+System.out.println(" The BigNumber = " + bigNumber);
+	}
+}
